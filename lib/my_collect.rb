ade5ca1b70a8a5d651afe448e60f5e09d689b401
callie rogers <callie1013@gmail.com>
2021-05-17 02:30:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,10 @@
-def my_collect(collection)
-  my_collect(array) do |name|
-    name.split(" ").first
+def my_collect(array)
+  i = 0
+  empty_array = []
+  while i < array.size
+    block_given?
+    empty_array << yield(array[i])
+    i += 1
   end
+  empty_array
 end 
